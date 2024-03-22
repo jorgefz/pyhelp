71c002e8ee962398459e03f540f1b8be85d94206
jorgefz <jorgefz.fernandez@gmail.com>
2024-03-22 17:12:26 +0000

    Added choice between printing to console and intercative help session

--- src/pyhelp/pyhelp.py
+++ src/pyhelp/pyhelp.py
@@ -2,6 +2,7 @@
 import sys
 import argparse
 import importlib
+import pydoc
 
 def pyhelp():
     parser = argparse.ArgumentParser(
@@ -10,13 +11,17 @@ def pyhelp():
         epilog = "Author: Jorge Fernandez. Check out the source code at github.com/jorgefz/pyhelp"
     )
     parser.add_argument('module')
+    parser.add_argument('-i', "--interactive", action="store_true", help="Open interactive session")
     args = parser.parse_args()
     
-    # try:
-    #     importlib.import_module()
-    
-    help(args.module)
+    # module_hierarchy = args.module.rsplit('.')
+    # module_name = module_hierarchy[0]
 
+    if args.interactive:    
+        help(args.module)
+    else:
+        docs = pydoc.render_doc(args.module, renderer=pydoc.plaintext)
+        print(docs)
 
 if __name__ == "__main__":
     pyhelp()
